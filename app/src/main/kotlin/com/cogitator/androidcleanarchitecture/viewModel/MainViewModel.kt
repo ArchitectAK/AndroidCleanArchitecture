7b8d5d841b5212607ee250019c336344b871146e
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2018-11-08 16:27:13 +0800

    adding splash

--- app/src/main/kotlin/com/cogitator/androidcleanarchitecture/viewModel/MainViewModel.kt
+++ app/src/main/kotlin/com/cogitator/androidcleanarchitecture/viewModel/MainViewModel.kt
@@ -1,10 +1,12 @@
 package com.cogitator.androidcleanarchitecture.viewModel
 
 import android.app.Application
+import com.cogitator.androidcleanarchitecture.base.BaseViewModel
 import javax.inject.Inject
 
 /**
  * @author Ankit Kumar on 08/10/2018
  */
-class MainViewModel  @Inject constructor(app: Application)
-    : BaseV(app), AccountRepository.AccountCallback {
+class MainViewModel @Inject constructor(app: Application)
+    : BaseViewModel(app), AccountRepository.AccountCallback {
+}
